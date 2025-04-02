f90c29037c2ef34e7ec31f2d0644fd3ee0fa32d5
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2025-04-02 17:35:18 +0200

    Add GenerateTask that shows the issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     id("java")
+    id("org.openapi.generator") version "7.12.0"
+
 }
 
 group = "org.example"
@@ -16,4 +20,52 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val generatedSourcesPath = "$projectDir/build/generated/sources/openapi"
+val generateJunoApi by
+tasks.registering(GenerateTask::class) {
+    val packageName = "com.ista.juno"
+    generatorName.set("java")
+    inputSpecRootDirectory = "$projectDir/src/main/resources/juno"
+    // inputSpecRootDirectorySkipMerge = false
+    outputDir.set(generatedSourcesPath)
+    library.set("webclient")
+    generateModelTests.set(false)
+    generateApiTests.set(false)
+
+    modelNameSuffix.set("Dto")
+    apiPackage.set(packageName)
+    modelPackage.set("$packageName.model")
+
+    configOptions.set(
+        mapOf(
+            "useJakartaEe" to "true",
+            "sourceFolder" to "api",
+            "useBeanValidation" to "true",
+            "useTags" to "true",
+        )
+    )
+
+    globalProperties.set(mapOf("skipFormModel" to "false"))
+
+    supportingFilesConstrainedTo =
+        listOf(
+            // base package
+            "ApiClient",
+            "JavaTimeFormatter",
+            "RFC3339DateFormat",
+            "ServerConfiguration",
+            "ServerVariable",
+            "StringUtil",
+
+            // auth
+            "ApiKeyAuth",
+            "Authentication",
+            "HttpBasicAuth",
+            "HttpBearerAuth",
+        )
+            .map { "$it.java" }
+    modelFilesConstrainedTo = listOf("")
+    apiFilesConstrainedTo = listOf("")
 }
